7c1660cf41b8de37fea8a663c33aee82a94a43f1
Tomas luquez <luquez1431@gmail.com>
2022-09-14 09:01:31 -0300

    mejore menu hamb y agrege boton simulador

--- main.js
+++ main.js
@@ -1,7 +1,26 @@
+// elementos guarados en variables //
 let menuBurger = document.getElementById("burger")
 let listado = document.getElementById("contenedor")
-let letras = document.getElementById("instruc")
+let letrasFondo = document.getElementById("instruc")
+let iconoQatar = document.getElementById("logo")
+let BotonSimulador = document.getElementById("simuButt")
+let listadoSmulador = document.getElementById("simuList")
+
+// funciones click //
 menuBurger.addEventListener("click", () => {
     listado.classList.toggle("listado")
-    letras.classList.toggle("opacity")
+    letrasFondo.classList.toggle("opacity")
 })
+
+iconoQatar.addEventListener("click", ()=> {
+    document.location="index.html"  
+})
+
+BotonSimulador.addEventListener("click", ()=> {
+    document.location="simulador.html"
+})
+
+listadoSmulador.addEventListener("click", ()=> {
+    document.location="simulador.html"
+})
+
