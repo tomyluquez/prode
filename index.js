4ba0295c77d71cc348c660ed69ebc1a45a620ec7
Tomas luquez <luquez1431@gmail.com>
2022-09-16 15:31:05 -0300

    agregue para entrar con el enter

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 let button = document.getElementById("buttonPass")
-let inputPass = document.getElementById("pass")
+// let inputPass = document.getElementById("pass")
+let form = document.getElementById("form")
 
-button.addEventListener("click", ()=> {
+form.addEventListener("submit", ingresar) // el evento submit es para que funcione con la tecla enter
+button.addEventListener("click", ingresar)
+
+function ingresar (evento) {
+    evento.preventDefault() //esto es para que mantenga el valor del input
     let passTomas = "tenista1"
     let passInput = document.getElementById("pass").value
 
-    if (passInput === passTomas) {
+     if (passInput === passTomas) {
         document.location="main.html"
-    } else {
-        alert("Contraseña incorrecta")
-    }
-})
+    } else alert("Contraseña incorrecta")
+}
+
